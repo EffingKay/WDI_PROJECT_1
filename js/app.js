dae664ae6182b16e0eab36558eadbb9fb7b19536
EffingKay <rozgonyiova.k@gmail.com>
2016-12-12 13:16:26 +0000

    yo

--- js/app.js
+++ js/app.js
@@ -62,7 +62,7 @@ setInterval(randomWord, interval);
 
 // Generates a random word and parse it to unordered list
 function randomWord() {
-  var requestStr = 'http://randomword.setgetgo.com/get.php';
+  var requestStr = 'https://randomword.setgetgo.com/get.php';
   $.ajax({
     type: 'GET',
     url: requestStr,
